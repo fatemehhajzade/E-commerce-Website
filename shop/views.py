3510c550e2e9f146b3a94822c54060c49f529608
Fateme Hajzade <fatemehjzd.03@gmail.com>
2025-09-20 12:22:29 +0330

    "first commit"

---
+++ shop/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,HttpResponse
+from .models import Product
+
+def helloworld(request):
+    # return HttpResponse("<h1>سلام دنیا</h1>")
+
+    # return render(request,'index.htm')
+
+    # return render(request,'index.html')
+
+    all_products = Product.objects.all()
+    return render(request,'index.html',{'products':all_products})
+    # return HttpResponse(all_products)

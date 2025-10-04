590ad8af37f53a4f367935070fc0e60adaceedbe
Fateme Hajzade <fatemehjzd.03@gmail.com>
2025-10-04 13:06:15 +0330

    menu in navbar

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
     path('signup/', views.signup_user, name="signup"),
-    path('product/<int:pk>', views.product, name="product")   
+    path('product/<int:pk>', views.product, name="product"),
+    path('category/<str:cat>', views.category, name="category")   
 ]
